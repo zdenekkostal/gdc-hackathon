a2999b7c6450596f1031f15f9fb412b70435082d
Petr Volny <petr.volny@gooddata.com>
2014-04-07 15:59:18 +0200

    Simple time to event start

--- client/scripts/app/controllers/GoodPointsController.js
+++ client/scripts/app/controllers/GoodPointsController.js
@@ -1,4 +1,4 @@
-define(["ember"], function(Ember){
+define(['ember', 'moment'], function(Ember, moment){
 	var GoodPointsController = Ember.Controller.extend({
 
         needs: ['progressBar'],
@@ -15,6 +15,14 @@ define(["ember"], function(Ember){
             return (max - min) * (progress);
         }.property('controllers.progressBar.progress'),
 
+        hasStarted: function() {
+            return moment("2014-04-10T08:00") < moment();
+        }.property('controllers.progressBar.progress'),
+
+        goodTime: function() {
+            var hasStarted = this.get('hasStarted');
+            return hasStarted ? moment("2014-04-11T11:00").fromNow() : moment("2014-04-10T08:00").fromNow();
+        }.property('controllers.progressBar.progress'),
     });
 	return GoodPointsController;
 });
