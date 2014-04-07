3e399fed96184321a18679da41819812ab8b62ce
Petr Volny <petr.volny@gooddata.com>
2014-04-07 13:33:15 +0200

    AllDataHackathon redesign

--- client/scripts/app/models/SchedulesModel.js
+++ client/scripts/app/models/SchedulesModel.js
@@ -1,116 +1,59 @@
 define(["ember"], function(Ember){
     var thursday = {
-        date: new Date(2013, 3, 18),
+        date: new Date(2014, 3, 10),
 
         actions: [
             {
-                start: new Date(2013, 3, 18, 8, 0),
-                title: 'Registration'
+                start: new Date(2014, 3, 10, 8, 0),
+                title: 'Registration & Warm up'
             },
 
             {
-                start: new Date(2013, 3, 18, 9, 0),
-                title: 'Breakfast'
+                start: new Date(2014, 3, 10, 9, 0),
+                title: 'Breakfast/Dinner'
             },
 
             {
-                start: new Date(2013, 3, 18, 10, 0),
+                start: new Date(2014, 3, 10, 10, 0),
                 title: 'Hackathon All-Hands'
             },
 
             {
-                start: new Date(2013, 3, 18, 12, 0),
+                start: new Date(2014, 3, 10, 11, 0),
                 title: 'Free Lunch'
             },
 
             {
-                start: new Date(2013, 3, 18, 16, 0),
-                title: 'Free Snacks'
-            },
-
-            {
-                start: new Date(2013, 3, 18, 19, 0),
-                title: 'Dinner time (OOO)'
-            },
-
-            {
-                start: new Date(2013, 3, 18, 23, 30),
-                title: 'Midnight pizza'
-            },
-
-            {
-                start: new Date(2013, 3, 19, 2, 0),
-                title: 'Scooter rallye'
-            },
-
-            {
-                start: new Date(2013, 3, 19, 3, 0),
-                title: 'GoodNap'
+                start: new Date(2014, 3, 10, 18, 0),
+                title: 'Dinner / GoodNight'
             }
         ]
     };
 
     var friday = {
-        date: new Date(2013, 3, 19),
+        date: new Date(2014, 3, 11),
 
         actions: [
             {
-                start: new Date(2013, 3, 19, 8),
-                title: 'Office run'
+                start: new Date(2014, 3, 11, 9),
+                title: 'Finish Breakfast / Dinner'
             },
 
             {
-                start: new Date(2013, 3, 19, 9),
-                title: 'Breakfast'
+                start: new Date(2014, 3, 11, 10),
+                title: 'Presentations'
             },
 
             {
-                start: new Date(2013, 3, 19, 12),
-                title: 'Free lunch'
-            },
-
-
-            {
-                start: new Date(2013, 3, 19, 14),
-                title: 'NERF Battle: Revenge'
-            },
-
-            {
-                start: new Date(2013, 3, 19, 16),
-                title: 'Free snacks'
-            },
-
-            {
-                start: new Date(2013, 3, 19, 18),
-                title: 'Beer time'
-            }
-        ]
-    };
-
-    var saturday = {
-        date: new Date(2013, 3, 20),
-
-        actions: [
-            {
-                start: new Date(2013, 3, 20, 9),
-                title: 'Morning sunshine'
-            },
-
-            {
-                start: new Date(2013, 3, 20, 10),
-                title: 'Afterparty on rails'
-            },
-
-            {
-                start: new Date(2013, 3, 20, 23),
-                title: 'The end'
+                start: new Date(2014, 3, 11, 11),
+                title: 'Friday Chillout / BeerTime / Afterparty'
             }
         ]
     };
 
     return [
         thursday,
-        friday,
-        saturday
+        friday
     ];
+
 });
